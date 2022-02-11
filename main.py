3a8ce70aea4070be8be09d332fe8b5663e6d8142
Cauê Machado Finger <77134560+cauefinger@users.noreply.github.com>
2022-02-10 22:08:24 -0300

    criando estrutura de login

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
- 
+loginAdministrador = "robocop"
+senhaAdministrador = "001"
+
+tentativas = 5
+
+while tentativas != 0:
+    login = input("Insira seu login: ")
+    senha = input("insira sua senha: ")
+
+    tentativas = tentativas - 1
+    if login == loginAdministrador and senha == senhaAdministrador:
+        print("Usuário autorizado")
+        exit()
+    else:
+        print("Usuário não autorizado")
+
