94b3c11b2e8e7dc1ba0194ec2bb02c67d22152b8
Cauê Machado Finger <77134560+cauefinger@users.noreply.github.com>
2022-02-10 22:21:45 -0300

    Criando opcoes e senhas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 loginAdministrador = "robocop"
 senhaAdministrador = "001"
+senhas = ["facebook: 123", "google: 456"]
 
 tentativas = 5
 
@@ -10,7 +11,16 @@ while tentativas != 0:
     tentativas = tentativas - 1
     if login == loginAdministrador and senha == senhaAdministrador:
         print("Usuário autorizado")
-        exit()
+        print("Opções: ")
+        print("1 - Listar senhas: ")
+        print("2 - Editar senha: ")
+        print("3 - Excluir senha: ")
+        opcao = int(input("Insira sua opção: "))
+
+        if opcao == 1:
+            print(senhas)
+
     else:
         print("Usuário não autorizado")
+        print("Tente novamente.")
 
